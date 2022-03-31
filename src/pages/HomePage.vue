c6b9763e1ce2dd55dd0bf6ab32c207a672f3dfe7
damouss <41292377+damouss@users.noreply.github.com>
2022-03-31 17:46:50 +0200

    add collections

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,20 +3,22 @@
         <Navbar/>
         <BannerLarge/>
         <SectionTwo/>
+        <CollectionsHF/>
     </div>
 </template>
 
 <script>
 import BannerLarge from "@/components/BannerLarge";
 import Navbar from "@/components/Navbar.vue";
-import SectionTwo from "@/components/SectionTwo.vue"
-
+import SectionTwo from "@/components/SectionTwo.vue";
+import CollectionsHF from "@/components/CollectionsHF.vue";
 export default {
     name: 'HomePage',
     components: {
         BannerLarge,
         Navbar,
-        SectionTwo
+        SectionTwo,
+        CollectionsHF
         },
     mounted() {
 
